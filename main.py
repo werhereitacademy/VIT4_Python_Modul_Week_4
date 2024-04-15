c700994d908dc82afc5041611f92a0e2aee17757
AbdullahMart <162364327+AbdullahMart@users.noreply.github.com>
2024-04-15 20:13:32 +0200

    Updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kitap_işlemleri
 import uye_işlemleri
 import json
+import zaman
 
 
 while True:
@@ -16,9 +17,9 @@ while True:
 |       copyright@vit4 group2  version 1.04        |
 |==================================================| """)
 
-    secim_menu= input("Lütfen Bir seçim yapiniz:")
+    ana_menu_secim= input("Lütfen Bir seçim yapiniz:")
 
-    if secim_menu == "1":
+    if ana_menu_secim == "1":
         print("""
 |=========Library  Management System=======[-][o][x]
 |        U Y E L I K   I S L E M L E R I N E       |
@@ -32,26 +33,36 @@ while True:
 |               version 1.04                       |       
 |           copyright@vit4 group2                  |
 |==================================================| """)
-        uyelik_menu= input("Lütfen Bir seçim yapiniz:")
-        if uyelik_menu=="1":
+        menu_uye_islemleri= input("Lütfen Bir seçim yapiniz:")
+
+        if menu_uye_islemleri=="1":
             pass
-        elif uyelik_menu=="2":
+
+        elif menu_uye_islemleri=="2":
+            uye_ekle(Uye_Adi,Tel,Adres)
+
+        elif menu_uye_islemleri=="3":
+            uye_ara(arama)
+
+        elif menu_uye_islemleri== "4":
+            uye_sil(silinecek_uye)
+
+        elif menu_uye_islemleri=="5":
+            kitap_odunc_verme()
+
+        elif menu_uye_islemleri=="6":
+            kitap_iade()
+
+        elif menu_uye_islemleri == "7":
             pass
-        elif uyelik_menu=="3":
-            pass
-        elif uyelik_menu== "4":
-            pass
-        elif uyelik_menu=="5":
-            pass
-        elif uyelik_menu=="6":
-            pass
-        elif uyelik_menu == "7":
-            pass
-        elif uyelik_menu =="0":
+        elif menu_uye_islemleri =="0":
+            print("Uye Islemlerinden cikis yaptiniz.")
             break
+        else:
+            print("Geçersiz seçim! Lütfen geçerli bir seçim yapınız.")
 
         
-    elif secim_menu == "2":
+    elif ana_menu_secim == "2":
         print("""
 |=========Library  Management System=======[-][o][x]
 |        K I T A P   İ Ş L E M L E R İ N E         |
@@ -65,29 +76,32 @@ while True:
 |               version 1.04                       |       
 |           copyright@vit4 group2                  |
 |==================================================| """)
-        kitap_menu= input("Lütfen Bir seçim yapiniz:")
+        menu_kitap_islemleri= input("Lütfen Bir seçim yapiniz:")
         
-        if kitap_menu=="1":
+        if menu_kitap_islemleri=="1":
             pass
-        elif kitap_menu=="2":
-            pass
-        elif kitap_menu=="3":
-            pass
-        elif kitap_menu== "4":
-            pass
-        elif kitap_menu=="5":
-            pass
-        elif kitap_menu=="6":
-            pass
-        elif kitap_menu == "7":
-            pass
-        elif kitap_menu =="0":
+
+        elif menu_kitap_islemleri=="2":
+            kitap_ekle(Kitap_Adi, Yazar, Yayinevi, Barkod)
+
+        elif menu_kitap_islemleri=="3":
+            kitap_arama(arama)
+        
+        elif menu_kitap_islemleri== "4":
+            kitap_sil(silinecek_veri)
+
+        elif menu_kitap_islemleri =="0":
+            print("Kitap Islemlerinden cikis yaptiniz.")
             break
+
+        else:
+            print("Geçersiz seçim! Lütfen geçerli bir seçim yapınız.")
        
-    elif secim_menu == "0":
+    elif ana_menu_secim == "0":
         print("Cikis yaptiniz.")
         print("Tekrar Kütüphanemize bekleriz..")
         break
+
     else:
         print("Geçersiz seçim! Lütfen geçerli bir seçim yapınız.")
 
