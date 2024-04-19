54a7701a1896cf146ddd9597f772e94e66d759e8
ERKHAN KOZHA <erkhankozha@gmail.com>
2024-04-19 13:00:19 +0200

    updated

--- Hackerrank_solution.py
+++ Hackerrank_solution.py
@@ -104,8 +104,6 @@ int main()
     return 0;
 }
 
-
-
 #Question 2:
 
 def rotateLeft(d, arr):
@@ -117,25 +115,6 @@ def rotateLeft(d, arr):
     return arr  
 
 #Question 3:
-
-
-
-#Question 1:
-
-
-
-#Question 2:
-
-def rotateLeft(d, arr):
-    for i in range(d):
-        temp=arr[0]
-        for j in range(len(arr)-1):
-            arr[j]=arr[j+1]
-        arr[len(arr)-1]=temp    
-    return arr  
-
-#Question 3:
-#!/bin/python3
 
 import math
 import os
@@ -143,12 +122,12 @@ import random
 import re
 import sys
 
-#
+
 # Complete the 'counterGame' function below.
 #
 # The function is expected to return a STRING.
 # The function accepts LONG_INTEGER n as parameter.
-#
+
 
 def counterGame(n):
     answer= ["Louise","Richard"]
@@ -182,7 +161,6 @@ if __name__ == '__main__':
 
 
 
-
 #Question 4:
 import math
 import os
@@ -215,36 +193,4 @@ if __name__ == '__main__':
 
 
 
-
-
-
-#Question 4:
-import math
-import os
-import random
-import re
-import sys
-from datetime import datetime
-# Complete the time_delta function below.
-def time_delta(t1, t2):
-    format_ = '%a %d %b %Y %H:%M:%S %z'
-    t1 = datetime.strptime(t1, format_)
-    t2 = datetime.strptime(t2, format_)
-    return str(int(abs((t1-t2).total_seconds()))) 
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    t = int(input())
-
-    for t_itr in range(t):
-        t1 = input()
-
-        t2 = input()
-
-        delta = time_delta(t1, t2)
-
-        fptr.write(delta + '\n')
-
-    fptr.close()
 
